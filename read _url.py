fbebcc6631724ca6e86bdf66a86849af8366c6ce
stepanve <stepan.ver11@gmail.com>
2017-09-02 18:18:24 +0900

    read html from url

---
+++ read _url.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 17:45:33 2017
+
+read url
+
+@author: stepan
+"""
+
+import urllib
+
+
+def read_url(url):
+    data = urllib.request.urlopen(url)
+    print(data.read().decode('utf-8'))
+    data.close()    
+    
+def main():
+    read_url('https://github.com/')   
+    
+if __name__ == "__main__":
+    main()
