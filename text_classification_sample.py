e6e7bf99f8b34192fb385518aedb5118114c59e8
stepanve <stepan.ver11@gmail.com>
2017-09-22 07:58:14 +0900

    料理店舗を分類できるように修正

--- text_classification_sample.py
+++ text_classification_sample.py
@@ -6,6 +6,10 @@ Created on Sun Sep  3 07:55:11 2017
 @author: stepan
 """
 
+
+import os
+print(os.listdir(path='.'))
+from normalization import normalize_corpus
 from feature_extractors import bow_extractor, tfidf_extractor
 from feature_extractors import averaged_word_vectorizer
 from feature_extractors import tfidf_weighted_averaged_word_vectorizer
@@ -31,37 +35,62 @@ def remove_empty_docs(corpus, labels):
     return filtered_corpus, filtered_labels
     
 import os
+print(os.listdir(path='.'))
+#os.chdir("./python-tool")
 
-# print(os.listdir(path='.'))
-# os.chdir("./python-tool")
+
+
 
 import google_search as gs
 import read_url
-urls = gs.google_search('王将')
-homepage_text = read_url.read_url(urls[0])
-
 import re
-p = re.compile(r"<[^>]*?>")
-homepage_text = p.sub("", homepage_text) 
-homepage_text = re.sub(r"\s|(&nbsp;)", "", homepage_text)
-
-from normalization import normalize_corpus
-homepage_text = homepage_text.strip()
-homepage_text = homepage_text.replace("\xc2\xa0", "")
-print(homepage_text)
-
 import get_tokens as gt
-data = gt.get_tokens(homepage_text)  
-print(data)  
+
+
+
+def setCorpus(searchString, label):
+    urls = gs.google_search(searchString)
+    homepage_text = read_url.read_url(urls[0])
+    p = re.compile(r"<[^>]*?>")
+    homepage_text = p.sub("", homepage_text) 
+    homepage_text = re.sub(r"\s|(&nbsp;)", "", homepage_text)
+    homepage_text = homepage_text.strip()
+    homepage_text = homepage_text.replace("\xc2\xa0", "")
+    data = gt.get_tokens(homepage_text)
+    return pd.DataFrame([[label, data]], columns =['target', 'data'])
+    
 
 dataset = pd.DataFrame(columns =['target', 'data'])
-df3 = pd.DataFrame([['中華料理', data]], columns =['target', 'data'])
-dataset = dataset.append(df3)
-dataset = dataset.append(df3)
-dataset = dataset.append(df3)
-dataset = dataset.append(df3)
-dataset = dataset.append(df3)
-dataset = dataset.append(df3)
+dataset = dataset.append(setCorpus('王将', '中華料理'))
+dataset = dataset.append(setCorpus('中国料理かなめ', '中華料理'))
+dataset = dataset.append(setCorpus('龍鳳 十三店', '中華料理'))
+dataset = dataset.append(setCorpus('中国料理 ファンファン', '中華料理'))
+dataset = dataset.append(setCorpus('台湾美食空間 一路發', '中華料理'))
+dataset = dataset.append(setCorpus('台湾・中華料理 詩宴（ＳＨＩＥＮ）', '中華料理'))
+dataset = dataset.append(setCorpus('バーミヤン', '中華料理'))
+dataset = dataset.append(setCorpus('中国菜オイル', '中華料理'))
+dataset = dataset.append(setCorpus('桃谷樓', '中華料理'))
+dataset = dataset.append(setCorpus('大阪聘珍樓', '中華料理'))
+dataset = dataset.append(setCorpus('プレスキル', 'フランス料理'))
+dataset = dataset.append(setCorpus('フレンチグリル・アンジェリック', 'フランス料理'))
+dataset = dataset.append(setCorpus('DEUX PLAISIR', 'フランス料理'))
+dataset = dataset.append(setCorpus('フランス料理 ワイズ', 'フランス料理'))
+dataset = dataset.append(setCorpus('ビストロ・コムシェトワ', 'フランス料理'))
+dataset = dataset.append(setCorpus('なごみの季 ダイニングレストラン', 'フランス料理'))
+dataset = dataset.append(setCorpus('西洋懐石アンシャンテ', 'フランス料理'))
+dataset = dataset.append(setCorpus('ＫＡＭＩＫＡＷＡ フランス料理', 'フランス料理'))
+dataset = dataset.append(setCorpus('俺のフレンチ 梅田', 'フランス料理'))
+dataset = dataset.append(setCorpus('ラターブルドモンクール', 'フランス料理'))
+dataset = dataset.append(setCorpus('肥後橋ゆきや。', '日本料理'))
+dataset = dataset.append(setCorpus('かじ井', '日本料理'))
+dataset = dataset.append(setCorpus('つのくに', '日本料理'))
+dataset = dataset.append(setCorpus('日本料理 四幸', '日本料理'))
+dataset = dataset.append(setCorpus('日本料理 花ゆう', '日本料理'))
+dataset = dataset.append(setCorpus('日本料理 魁', '日本料理'))
+dataset = dataset.append(setCorpus('日本料理 くろ川', '日本料理'))
+dataset = dataset.append(setCorpus('はなの', '日本料理'))
+dataset = dataset.append(setCorpus('隨縁亭 日本料理', '日本料理'))
+dataset = dataset.append(setCorpus('日本料理 北新地 湯木 新店', '日本料理'))
 
 
 corpus, labels = dataset.data, dataset.target
@@ -73,13 +102,15 @@ print('Class label:',labels[0])
 train_corpus, test_corpus, train_labels, test_labels = prepare_datasets(corpus,
                                                                         labels,
                                                                         test_data_proportion=0.3)
-                                          
-                                                     
-#from normalization import normalize_corpus
 
-
-print(train_corpus)
+test_dataset = pd.DataFrame(columns =['target', 'data'])
+test_dataset = test_dataset.append(setCorpus('王将', '中華料理'))
+test_corpus = test_dataset.data
+test_labels = test_dataset.target
 print(test_corpus)
+print(test_labels)                                                 
+
+
 norm_train_corpus = normalize_corpus(train_corpus)
 norm_test_corpus = normalize_corpus(test_corpus)  
 
@@ -99,6 +130,7 @@ tokenized_train = [nltk.word_tokenize(text)
                    for text in norm_train_corpus]
 tokenized_test = [nltk.word_tokenize(text)
                    for text in norm_test_corpus]
+
 # build word2vec model                   
 model = gensim.models.Word2Vec(tokenized_train,
                                size=500,
@@ -154,7 +186,9 @@ def get_metrics(true_labels, predicted_labels):
                                                predicted_labels,
                                                average='weighted'),
                         2))
-                        
+
+
+
 
 def train_predict_evaluate_model(classifier, 
                                  train_features, train_labels, 
@@ -182,6 +216,7 @@ mnb_bow_predictions = train_predict_evaluate_model(classifier=mnb,
                                            train_labels=train_labels,
                                            test_features=bow_test_features,
                                            test_labels=test_labels)
+
 
 # Support Vector Machine with bag of words features
 svm_bow_predictions = train_predict_evaluate_model(classifier=svm,
@@ -218,23 +253,21 @@ svm_tfidfwv_predictions = train_predict_evaluate_model(classifier=svm,
                                            test_features=tfidf_wv_test_features,
                                            test_labels=test_labels)
 
+print(mnb_tfidf_predictions)
  
 
 cm = metrics.confusion_matrix(test_labels, svm_tfidf_predictions)
-pd.DataFrame(cm, index=range(0,20), columns=range(0,20))  
+pd.DataFrame(cm, index=range(0,3), columns=range(0,3))  
 
-class_names = dataset.target_names
-print(class_names[0], '->', class_names[15])
+class_names = dataset.target
+print(class_names[0], '->', class_names[0])
 print(class_names[18], '->', class_names[16])
 print(class_names[19], '->', class_names[15])
 
 
 
-import re
-
 num = 0
 for document, label, predicted_label in zip(test_corpus, test_labels, svm_tfidf_predictions):
-    if label == 0 and predicted_label == 15:
         print('Actual Label:', class_names[label])
         print('Predicted Label:', class_names[predicted_label])
         print('Document:-')
